378cf97080faa6bb3d792e5c0395f778a54a1c1f
drbeefsupreme <drbeefsupreme@discordja.net>
2020-12-25 14:07:36 -0500

    testcommit

--- .config/nixpkgs/user/apps.nix
+++ .config/nixpkgs/user/apps.nix
@@ -10,25 +10,4 @@
     steam
     vlc
   ];
-
-  ####################################################################################################
-  # systemd.user = {                                                                                 #
-  #   services = {                                                                                   #
-  #     dropbox = {                                                                                  #
-  #       Unit = {                                                                                   #
-  #         Description = "Dropbox";                                                                 #
-  #         After = [ "graphical-session-pre.target" ];                                              #
-  #         PartOf = [ "graphical-session.target" ];                                                 #
-  #       };                                                                                         #
-  #       Service = {                                                                                #
-  #         Restart = "on-failure";                                                                  #
-  #         RestartSec = 1;                                                                          #
-  #         ExecStart = "${pkgs.dropbox}/bin/dropbox";                                               #
-  #         Environment = "QT_PLUGIN_PATH=/run/current-system/sw/${pkgs.qt5.qtbase.qtPluginPrefix}"; #
-  #       };                                                                                         #
-  #       Install = {                                                                                #
-  #         WantedBy = [ "graphical-session.target" ];                                               #
-  #       };                                                                                         #
-  #     };                                                                                           #
-  ####################################################################################################
 }
